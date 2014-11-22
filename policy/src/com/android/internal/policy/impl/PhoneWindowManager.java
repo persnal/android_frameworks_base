7b7dedb675de31e7438c4c9911235d23a2d56ab4
Cafe in bread <whqjarn111@naver.com>
2014-11-22 22:13:35 +0900

    Update PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -721,6 +721,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.DEV_FORCE_SHOW_NAVBAR), false, this,
                     UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
++                    Settings.System.NAVIGATION_BAR_HEIGHT), false, this,
++                    UserHandle.USER_ALL);
 
             updateSettings();
         }
